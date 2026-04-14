9644fea5e402f75ebf56872a074587671d377daf
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2026-04-14 16:25:30 +0900

    Docs: 식재료 수동 추가 api 스웨거 설명 수정

--- src/main/java/com/team200/graduation_project/domain/admin/controller/AdminControllerDocs.java
+++ src/main/java/com/team200/graduation_project/domain/admin/controller/AdminControllerDocs.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Tag(name = "Admin", description = "관리자 관련 API")
 public interface AdminControllerDocs {
 
-    @Operation(summary = "식재료 수동 추가 API", description = "관리자가 식재료를 수동으로 등록합니다.")
+    @Operation(summary = "식재료 수동 추가", description = "관리자가 식재료를 수동으로 등록합니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error")
