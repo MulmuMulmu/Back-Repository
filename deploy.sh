8035e929a09ceb328bbd5a4f2904f829a16f78a2
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2026-04-23 15:15:23 +0900

    Chore: 무중단배포 1차 테스트

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 IS_NGINX_UP=$(docker ps | grep nginx)
 if [ -z "$IS_NGINX_UP" ]; then
     echo ">>> Starting Nginx..."
-    docker-compose up -d nginx
+    docker compose up -d nginx
 fi
 
 # 어느 컨테이너가 실행 중인지 확인
@@ -13,7 +13,7 @@ IS_BLUE=$(docker ps | grep blue)
 if [ -z "$IS_BLUE" ]; then
   # Blue가 없으면 Blue를 시작
   echo ">>> Blue deployment starting..."
-  docker-compose up -d blue
+  docker compose up -d blue
   BEFORE_COLOR="green"
   AFTER_COLOR="blue"
   BEFORE_PORT=8081
@@ -21,7 +21,7 @@ if [ -z "$IS_BLUE" ]; then
 else
   # Blue가 있으면 Green을 시작
   echo ">>> Green deployment starting..."
-  docker-compose up -d green
+  docker compose up -d green
   BEFORE_COLOR="blue"
   AFTER_COLOR="green"
   BEFORE_PORT=8080
@@ -30,9 +30,8 @@ fi
 
 # 새 컨테이너 헬스 체크
 echo ">>> Waiting for $AFTER_COLOR to be healthy..."
-for i in {1..15} # 시간을 조금 더 늘렸습니다.
+for i in {1..15}
 do
-  # /swagger-ui/index.html 대신 간단한 루트 경로도 좋습니다.
   HEALTH=$(curl -s http://localhost:$AFTER_PORT/swagger-ui/index.html)
   if [ -n "$HEALTH" ]; then
     echo ">>> $AFTER_COLOR is healthy!"
@@ -44,20 +43,20 @@ done
 
 if [ -z "$HEALTH" ]; then
   echo ">>> $AFTER_COLOR health check failed. Aborting."
-  docker-compose stop $AFTER_COLOR
+  docker compose stop $AFTER_COLOR
   exit 1
 fi
 
 # Nginx 트래픽 전환
 echo ">>> Switching traffic to $AFTER_COLOR..."
 echo "set \$service_url http://$AFTER_COLOR:$AFTER_PORT;" > ./nginx/conf.d/service-url.inc
-docker-compose exec -T nginx nginx -s reload
+docker compose exec -T nginx nginx -s reload
 
 # 구버전 컨테이너 종료 (동작 중인 경우에만)
 IS_BEFORE_UP=$(docker ps | grep $BEFORE_COLOR)
 if [ -n "$IS_BEFORE_UP" ]; then
     echo ">>> Stopping $BEFORE_COLOR..."
-    docker-compose stop $BEFORE_COLOR
+    docker compose stop $BEFORE_COLOR
 fi
 
 echo ">>> Deployment completed successfully!"
