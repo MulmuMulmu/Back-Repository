212ffa149e0eb28ab7d77e7ba5f158ed9d98d11d
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2026-04-23 17:40:09 +0900

    Feat: 로드밸런서 헬스체크 api 추가

--- src/main/java/com/team200/graduation_project/global/test/testController.java
+++ src/main/java/com/team200/graduation_project/global/test/testController.java
@@ -3,6 +3,7 @@ package com.team200.graduation_project.global.test;
 import com.team200.graduation_project.global.apiPayload.ApiResponse;
 import com.team200.graduation_project.global.apiPayload.code.GeneralErrorCode;
 import com.team200.graduation_project.global.apiPayload.exception.GeneralException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-    @GetMapping("/test/health-check")
-    public ApiResponse<?> get() {
-        return ApiResponse.onSuccess("server is running");
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 
     @PostMapping("/test/custom-exception")
@@ -24,6 +25,5 @@ public class testController {
         }
         return ApiResponse.onSuccess("로그인 성공!");
     }
-
 
 }
